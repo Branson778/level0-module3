c0681b78b86d8e8c8192253ceeb50177f9d07526
root <root@fba1c4195f9b>
2020-09-05 23:16:32 +0000

    Sat. 1

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -46,7 +46,18 @@ public static void main(String[] args) {
 	//JOptionPane.showMessageDialog(null, "Buzz");
 for (int i = 1; i < 21; i++) {
 
-	JOptionPane.showMessageDialog(null, i);
+	if (i%3==0 && i%5==0) {
+		System.out.println("Fizzbuzz");
+	}
+	else if (i%3==0) {
+		System.out.println("Fizz");
+	}
+	else if (i%5==0) {
+		System.out.println("Buzz");
+	}
+	else {
+		System.out.println(i);
+	}
 }
 	
 }
